cdc34e2fa3d693101ea2cc414731ebc4641fe1b9
Zachary Julius Sultan <ZACHARYJSULTAN@GMAIL.COM>
2022-08-02 11:34:51 -0700

    dev

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 // import functions and grab DOM elements
+const questionInputEl = document.getElementById('question-input');
+const optionAInputEl = document.getElementById('option-a-input');
+const optionBInputEl = document.getElementById('option-b-input');
 
+const optionAYesEl = document.getElementById('option-a-yes');
+const optionBYesEl = document.getElementById('option-b-yes');
+
+const optionANoEl = document.getElementById('option-a-no');
+const optionBNoEl = document.getElementById('option-b-no');
+
+const addPollEl = document.querySelector('#add-poll');
+const archivePollEl = document.querySelector('#archive-poll');
 // let state
 
 // set event listeners 
