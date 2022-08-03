399ed96f5408b908d365d6fccf379dfc1c5c4c6d
Zachary Julius Sultan <ZACHARYJSULTAN@GMAIL.COM>
2022-08-03 10:58:19 -0700

    dev

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 // import functions and grab DOM elements
-const questionInputEl = document.getElementById('question-input');
-const optionAInputEl = document.getElementById('option-a-input');
-const optionBInputEl = document.getElementById('option-b-input');
+import { renderPoll } from './render-poll.js';
 
 const optionAYesEl = document.getElementById('option-a-yes');
 const optionBYesEl = document.getElementById('option-b-yes');
@@ -9,11 +7,100 @@ const optionBYesEl = document.getElementById('option-b-yes');
 const optionANoEl = document.getElementById('option-a-no');
 const optionBNoEl = document.getElementById('option-b-no');
 
-const addPollEl = document.querySelector('#add-poll');
-const archivePollEl = document.querySelector('#archive-poll');
+const addPollEl = document.getElementById('add-poll');
+const archivePollEl = document.getElementById('archive-poll');
+const currentPollEl = document.getElementById('newest-poll');
+const pastPollEl = document.getElementById('past-polls');
+
+const pollForm = document.getElementById('pollform');
 // let state
+let pastPolls = [];
+
+let question = '';
+let optionA = '';
+let optionB = '';
+let votesA = 0;
+let votesB = 0;
+
 
 // set event listeners 
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+pollForm.addEventListener('submit', (event) => {
+    currentPollEl.textContent = '';
+    event.preventDefault();
+    votesA = 0;
+    votesB = 0;
+    const data = new FormData(pollForm);
+
+    const userQuestion = data.get('question');
+    const userOptionA = data.get('option-a');
+    const userOptionB = data.get('option-b');
+
+    question = userQuestion;
+    optionA = userOptionA;
+    optionB = userOptionB;
+
+  
+    displayCurrentPoll(); 
+});
+
+optionAYesEl.addEventListener('click', () => {
+    votesA++;
+    renderPoll();
+    displayCurrentPoll();
+});
+optionBYesEl.addEventListener('click', () => {
+    votesB++;
+    renderPoll();
+    displayCurrentPoll();
+});
+optionANoEl.addEventListener('click', () => {
+    votesA--;
+    renderPoll();
+    displayCurrentPoll();
+});
+optionBNoEl.addEventListener('click', () => {
+    votesB--;
+    renderPoll();
+    displayCurrentPoll();
+});
+
+addPollEl.addEventListener('click', () => {
+
+    const currentPoll = {
+        question: question,
+        optionA: optionA,
+        optionB: optionB,
+        votesA: votesA,
+        votesB: votesB,
+    };
+
+  
+    pastPolls.push(currentPoll);
+    DisplayAllPolls(pastPolls);
+
+});
+
+function displayCurrentPoll() {
+    currentPollEl.textContent = '';
+
+    renderPoll();
+
+}
+
+function DisplayAllPolls() {
+
+    currentPollEl.textContent = '';
+    pastPollEl.textContent = '';
+
+    for (let poll of pastPolls) {
+      
+        const pastPollsRender = renderPoll(poll.question, poll.votesA, poll.votesB, poll.optionA, poll.optionB);
+
+        pastPollEl.append(pastPollsRender);
+    }
+
+}
+
---
+++ render-poll.js
@@ -0,0 +1,8 @@
+export function renderPoll(question, optionA, optionB, votesA, votesB) {
+
+    const addPollEl = document.createElement('div');
+    
+    addPollEl.append(pollForm);
+    
+    return 
+}
